581823e31663035ce54a4036eaa16a7305e453f9
unknown <cook2421>
2020-03-19 21:22:34 +0900

    9장까지 완료

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,11 +1,10 @@
-public class Dollar{
-    public int amount;
+public class Dollar extends Money {
 
-    public Dollar(int amount){
-        this.amount = amount;
+    Dollar(int amount, String currency){
+        super(amount, currency);
     }
 
-    public Dollar times(int multiplier){
-        return new Dollar(amount * multiplier);
+    public Money times(int multiplier){
+        return Money.dollar(amount * multiplier);
     }
 }
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,15 +1,28 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class DollarTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
